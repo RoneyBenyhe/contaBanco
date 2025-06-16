cdd3f5a482da39db8a16f26078d166398525d764
Roney Benyhe Rodrigues <roneybenyhebrs@gmail.com>
2025-06-16 14:30:37 -0300

    projeto-dio-completo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número da sua conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o seu saldo:");
+        float saldo = scanner.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.%n", nome, agencia, conta, saldo);
     }
 }
